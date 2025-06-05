e56d9f34ba1b3a58ee492269b0bff9ac13ae25a0
Rick Rodriguez <rickrod6502@gmail.com>
2025-06-05 01:35:37 +0000

    Created Invoice table, /token route

--- src/api/models.py
+++ src/api/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Float, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
@@ -10,10 +10,29 @@ class User(db.Model):
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    # relationships
+    invoice_list: Mapped[list["Invoice"]] = relationship(back_populates='user')
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+    
+class Invoice(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    invoice_date: Mapped[str] = mapped_column(String(120), nullable=False)
+    invoice_number: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    invoice_amount: Mapped[float] = mapped_column(Float(), nullable=False)
+
+    # relationships
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates='invoice_list')
+
+    def serialize(self):
+        return {
+            "invoice_date": self.invoice_date,
+            "invoice_number": self.invoice_number,
+            "invoice_amount": self.invoice_amount,
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,9 +2,12 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Invoice
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 api = Blueprint('api', __name__)
 
@@ -12,11 +15,52 @@ api = Blueprint('api', __name__)
 CORS(api)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/token", methods=["POST"])
+def generate_token():
+    
+    # login credentials
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+    # query the DB to check if the user email exists
+    email = email.lower()
+    user = User.query.filter_by(email=email).first()
+
+    # create a condition if the user does NOT exist or if the password is wrong
+    if user is None:
+        raise APIException('Sorry email or password not found', status_code=404)
+    elif user is not None and user.password != password:
+        raise APIException('Sorry email or password not found', status_code=404)
+    
+    # the user DOES exist and the passwords matched
+    access_token = create_access_token(identity=user.id)
+
+    response = {
+        'access_token': access_token,
+        'user_id': user.id,
+        'message': f'Welcome {user.email}!'
     }
 
-    return jsonify(response_body), 200
+    return jsonify(response), 200
+
+
+# HW
+# create a signup route and test it on postman
+# you will receive the email and a password
+# check if the email already exists
+# if it does not exist create a new User()
+# send a jsonified response letting the user know that they have successfully signed up
+
+
+
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    return jsonify(logged_in_as=current_user), 200
